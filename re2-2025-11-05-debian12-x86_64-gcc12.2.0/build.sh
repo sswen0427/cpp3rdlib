30671f432a7076bfa5e4514dc62fcd90c03e0647
sswen0427 <799438891@qq.com>
2026-04-18 13:38:31 +0800

    add re2

---
+++ re2-2025-11-05-debian12-x86_64-gcc12.2.0/build.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/google/re2.git
+cd re2
+git checkout 2025-11-05
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=re2_install \
+    -DCMAKE_PREFIX_PATH=/home/wensisi.0427/cpp3rdlib/abseil-cpp-20260107.1-debian12-x86_64-gcc12.2.0
+make -j$(nproc)
+make install
+mv re2_install/ ../..
+cd ../..
+rm -rf re2
+mv re2_install re2
